5455cb7c1f5784b3804121393c67fb50bd9e634c
brookeperkins <brookemmperkins@gmail.com>
2021-03-23 21:06:12 -0700

    types, arrays, and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,32 +1,44 @@
 // // let character = 'brooke'
 // // let age = 30;
 // // let isBlackBelt = false;
+
 // // character = 'you know who';
+
 // // const circ = (diameter: number) => {
 // //   return diameter * Math.PI;
 // // }
+
 // //console.log(circ('40')); <-- won't work
 // // console.log(circ(40)); <-- works!
+
 // //arrays 
 // let names = ['brooke', 'jacob', 'nomi'];
 // names.push('frog');
+
 // let numbers = [10, 20, 40, 80];
+
 // // numbers.push(30); <-- works
 // // numbers.push('30'); <-- nope
 // // numbers[1] = '30'; <-- nope
+
 // let mixedArray = [10, 'ten', 'this', 8, 9, false];
+
 // // mixedArray.push('hellllooo'); <-- works because type is included in array above
 // // mixedArray.push(5); <-- same
 // // mixedArray[0] = 'YOOO'; <-- same
+
 // let ninja = {
 //   name: 'ralph',
 //   belt: 'black',
 //   age: 30
 // };
+
 // ninja.age = 1000;
 // ninja.name = 'brooke';
 // // ninja.age = 'thirty' <-- does not workkkk
+
 // // ninja.skills = ['dancing', 'knife'] <-- does not work because there was no skills on the original Array
+
 // ninja = {
 //   name: 'okay',
 //   belt: 'green', 
