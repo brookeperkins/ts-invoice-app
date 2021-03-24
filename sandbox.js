223aa7b6e1856c178b50df26b2bf015d38201a15
brookeperkins <brookemmperkins@gmail.com>
2021-03-23 21:29:21 -0700

    ANY types

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,15 @@
-//explicit types
-var character;
-var age;
-var isLoggedIn;
-// age = 'luigi'; <-- nope
-age = 30;
-// isLoggedIn = 25; <-- nope
-isLoggedIn = true;
-//arrays
-var ninjas = [];
-//initialize with a value in order to do .push because without = [] it's not initialized, just defining the type!
-// ninjas = [10, 23]; <--nope
-ninjas.push('ninja1', 'bruce lee');
-//union types
+//any types - be wary of using these!! removes usefulness of ts, honestly
+//below is not equal to a number, still equal to any
+var age = 25;
+// this all works
+age = true;
+age = 'hello';
+age = { name: 'maria' };
+console.log(age);
 var mixedArray = [];
-mixedArray.push('helloooo');
-mixedArray.push(50);
-// mixedArray.push(false); <--- doesn't work because we didn't include it in the union type above
-//(string|number|boolean) to get it to work
-//only use () when types are in front of an array
+mixedArray.push(5);
+mixedArray.push('hello');
+mixedArray.push(false);
 console.log(mixedArray);
-var hello;
-hello = 'hey';
-hello = 43110;
-// hello = false; <-- nahhh
-//objects
-var goodbye;
-goodbye = {
-    "this": 'that',
-    those: 40
-};
-goodbye = {
-    "this": 'maybe',
-    those: 100
-};
-//can set an object to have specific types, must explicitly have all these key-value pairs with these types
-var okay;
+var newArraything;
+console.log(newArraything);
--- sandbox.ts
+++ sandbox.ts
@@ -1,51 +1,20 @@
-//explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+//any types - be wary of using these!! removes usefulness of ts, honestly
+//below is not equal to a number, still equal to any
+let age: any = 25;
 
-// age = 'luigi'; <-- nope
-age = 30;
+// this all works
+age = true;
+age = 'hello';
+age = { name: 'maria'};
 
-// isLoggedIn = 25; <-- nope
-isLoggedIn = true;
+console.log(age);
 
-//arrays
-let ninjas: string[] = []; 
-//initialize with a value in order to do .push because without = [] it's not initialized, just defining the type!
-// ninjas = [10, 23]; <--nope
+let mixedArray: any[] =[];
 
-ninjas.push('ninja1', 'bruce lee');
-
-//union types
-let mixedArray: (string | number)[] = [];
-mixedArray.push('helloooo');
-mixedArray.push(50);
-// mixedArray.push(false); <--- doesn't work because we didn't include it in the union type above
-//(string|number|boolean) to get it to work
-//only use () when types are in front of an array
+mixedArray.push(5);
+mixedArray.push('hello');
+mixedArray.push(false);
 console.log(mixedArray);
 
-let hello: number|string;
-hello = 'hey';
-hello = 43110;
-// hello = false; <-- nahhh
-
-//objects
-let goodbye: object;
-goodbye = {
-  this: 'that',
-  those: 40
-}
-
-goodbye = {
-  this: 'maybe',
-  those: 100
-}
-
-//can set an object to have specific types, must explicitly have all these key-value pairs with these types
-let okay: {
-  age: number,
-  name: string,
-  experience: boolean  
-}
-
+let newArraything: {name: any, age: any};
+console.log(newArraything);
